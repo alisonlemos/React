d1b7a0d6db949057d03f9511a1015cd79b1a3f25
Alison <alisongarantizado@gmail.com>
2020-09-10 13:55:51 -0400

    Assignment 3

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem ,
-        Button, Modal, ModalHeader, ModalBody, Label, FormGroup } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, Label, FormGroup } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-import { Link } from 'react-router-dom';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -20,6 +19,7 @@ class CommentForm extends Component {
         };
     }
 
+
     toggleModal() {
         this.setState({
           isModalOpen: !this.state.isModalOpen
@@ -30,6 +30,7 @@ class CommentForm extends Component {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
     }
+
 
     render() {
         return(
@@ -89,59 +90,7 @@ class CommentForm extends Component {
             </Modal>
         </>)
     }
+
 }
 
-function RenderDish({dish}) {
-    return dish ? (
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-    ): <div></div>
-}
-
-function RenderComments({comments}) {
-    return comments ? (
-        <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-                {comments.map((comment)=>(
-                    <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>{`-- ${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}</p>
-                    </li>
-                ))}
-            </ul>
-            <CommentForm/>
-        </div>
-    ) : <div></div>
-}
-
-const  DishDetail = (props) => {
-        return( 
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-        ) 
-}
-
-export default DishDetail;
+export default CommentForm;
